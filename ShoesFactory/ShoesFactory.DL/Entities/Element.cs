9c6a74191ad96d9f4e44c9db63ac482f2ecb0ea1
Yevhenii Kovalenko <jeikkowalenko@gmail.com>
2019-09-22 14:52:49 +0300

    Added entities to DAL

---
+++ ShoesFactory/ShoesFactory.DL/Entities/Element.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoesFactory.DL.Entities
+{
+    public class Element
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Summary { get; set; }
+        public IEnumerable<Material> Materials { get; set; }
+        public double Price { get; set; }
+    }
+}
---
+++ ShoesFactory/ShoesFactory.DL/Entities/Shoes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoesFactory.DL.Entities
+{
+    public class Shoes
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Summary { get; set; }       
+        public double Margin { get; set; }
+        public IEnumerable<Element> Elements { get; set; }
+    }
+}
